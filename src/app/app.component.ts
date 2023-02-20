9ab765e66656283a54e92c36279316b49e391004
TheBakari <draganic69@gmail.com>
2023-02-20 14:17:52 +0100

    Employee class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from './homework3/employes.model';
 import { Candidate } from './homework3/candidate.model';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,40 +10,54 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'typescritpApp';
   ngOnInit(): void {
-    const candidate = new Candidate(1, 'Marko', 'Markovic', '20-5-1993');
-    const candidate2 = new Candidate(2, 'Nikola', 'Nikolic', '19-04-1992');
+    // const candidate = new Candidate(1, 'Marko', 'Markovic', '20-5-1993');
+    // const candidate2 = new Candidate(2, 'Nikola', 'Nikolic', '19-04-1992');
+    // // console.log(candidate);
+    // // console.log(candidate2);
+    // const arrCand: Candidate[] = [
+    //   {
+    //     id: 1,
+    //     firstName: 'Jovana',
+    //     lastName: 'Jovanovic',
+    //     birthday: '19-03-1993',
+    //   },
+    //   {
+    //     id: 1,
+    //     firstName: 'Mila',
+    //     lastName: 'Milovanovic',
+    //     birthday: '19-03-1993',
+    //   },
+    //   {
+    //     id: 1,
+    //     firstName: 'Matija',
+    //     lastName: 'Jovanovic',
+    //     birthday: '19-03-1993',
+    //   },
+    // ];
+    // console.log(arrCand);
+    // function disp() {
+    //   return arrCand;
+    // }
+    // let display = disp();
+    // for (let i in display) {
+    //   console.log(display[i]);
+    // }
 
-    // console.log(candidate);
-    // console.log(candidate2);
-
-    const arrCand: Candidate[] = [
-      {
-        id: 1,
-        firstName: 'Jovana',
-        lastName: 'Jovanovic',
-        birthday: '19-03-1993',
-      },
-      {
-        id: 1,
-        firstName: 'Mila',
-        lastName: 'Milovanovic',
-        birthday: '19-03-1993',
-      },
-      {
-        id: 1,
-        firstName: 'Matija',
-        lastName: 'Jovanovic',
-        birthday: '19-03-1993',
-      },
-    ];
-    //console.log(arrCand);
-
-    function disp() {
-      return arrCand;
-    }
-    let display = disp();
-    for (let i in display) {
-      console.log(display[i]);
-    }
+    const emp = new Employee(
+      1,
+      'Marko',
+      'markomarkovic@gmail.com',
+      +38161222333,
+      'Programmer'
+    );
+    const emp2 = new Employee(
+      1,
+      'Jovan',
+      'Jovanovic@gmail.com',
+      +38161222333,
+      'Programmer'
+    );
+    console.log(emp);
+    console.log(emp2);
   }
 }
